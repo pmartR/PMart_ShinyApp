da48ad7cd84be1eb11c16e0ae9fae69974581f92
Daniel Claborne <daniel.claborne@pnnl.gov>
2021-09-21 22:17:14 +0000

    Add fold change and interactivity to statsres plots

--- UI_elements/peptide_analysis_UI.R
+++ UI_elements/peptide_analysis_UI.R
@@ -1,15 +1,15 @@
 # main plot display which takes in the plot object that is created immediately after imd_anova() is run
-output$peptide_statistics_mainplot <- renderPlot({
+output$peptide_statistics_mainplot <- renderUI({
   req(!is.null(plots$peptide_statistics_mainplot))
   p <- plots$peptide_statistics_mainplot
-  if (inherits(p, "list")) {
-    p <- gridExtra::arrangeGrob(p[[1]], p[[2]], ncol = 2)
-    plots$last_plot <- p
-    grid::grid.draw(p)
+  plots$last_plot <- p
+  if (inherits(p, "plotly")) {
+    output$peptide_statistics_mainplot_plotly <- renderPlotly(p)
+    plotlyOutput("peptide_statistics_mainplot_plotly")
   }
-  else {
-    plots$last_plot <- p
-    return(p)
+  else if (inherits(p, "ggplot")) {
+    output$peptide_statistics_mainplot_ggplot <- renderPlot(p)
+    plotOutput("peptide_statistics_mainplot_ggplot")
   }
 })
 
@@ -228,10 +228,52 @@ output$peptide_statistics_summary_table <- renderDT({
   objects$peptide_imdanova_res[["Full_results"]]
 }, options = list(scrollX =TRUE))
 
-# theme UI
+#'@details UI created with the helper function style_UI to edit plot options
+#'in plot.<object> function as well as axes values (if it is a ggplot)
 output$peptide_statistics_plot_options <- renderUI({
-  style_UI("statistics")
+  cpicker_args <- list(
+    list(inputId = "pep_imd_down_cpicker", label = "Negative", value = "red"),
+    list(inputId = "pep_imd_nonsig_cpicker", label = "Non significant", value = "black"),
+    list(inputId = "pep_imd_up_cpicker", label = "Positive", value = "green")
+  )
+  
+  tagList(
+    h4("Main plot content"),
+    conditionalPanel(
+      "input.peptide_imdanova_plot_type == 'volcano'",
+      div(
+        class = "inline-wrapper-1",
+        numericInput("peptide_imd_plot_fc_thresh", "Fold-change Threshold", value = NULL),
+        radioGroupButtons(
+          "peptide_stats_interactive_yn",
+          choices = c("Interactive" = T, "Static" = F),
+          selected = F
+        )
+      )
+    ),
+    h5("Statistical significance/fold change colors"),
+    inline_cpickers(cpicker_args), # UI helper
+    div(
+      class = "inline-wrapper-1",
+      actionButton("peptide_stats_update_plot_content", "Update plot"),
+      conditionalPanel("input.peptide_stats_interactive_yn == 'TRUE'",
+                       tipify(blueexcl, ttext_[["IMD_INTERACTIVE_MANY_POINTS"]]))
+    ),
+    conditionalPanel(
+      "input.peptide_stats_interactive_yn != 'TRUE'",
+      
+      tagList(
+        style_UI(
+          "peptide_statistics",
+          hr(),
+          h4("Axes styling")
+        ),
+        uiOutput("peptide_statistics_apply_style")
+      )
+    )
+  )
 })
+
 output$peptide_statistics_apply_style <- renderUI({
-  apply_style_UI("statistics", FALSE, inherits(plots$peptide_statistics_mainplot, "list"))
+  apply_style_UI("peptide_statistics", FALSE, inherits(plots$peptide_statistics_mainplot, "list"))
 })
--- tabs_UI/peptide_analysis_tab.R
+++ tabs_UI/peptide_analysis_tab.R
@@ -38,12 +38,11 @@ peptide_statistics_UI <- function() {
                bsCollapsePanel("Plots",
                                value = "peptide_statistics_plots",
                                radioGroupButtons("peptide_imdanova_plot_type", "Plot type", choices = c("Bar" = "bar", "Volcano" = "volcano")),
-                               withSpinner(plotOutput("peptide_statistics_mainplot"))
+                               withSpinner(uiOutput("peptide_statistics_mainplot"))
                ),
                bsCollapsePanel("Plot Options",
                                value = "peptide_statistics_plot_opts",
-                               uiOutput("peptide_statistics_plot_options"),
-                               uiOutput("peptide_statistics_apply_style")
+                               uiOutput("peptide_statistics_plot_options")
                ),
                bsCollapsePanel("Tables",
                                value = "peptide_statistics_tables",
