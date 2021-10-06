075bc4896579265074ce073f2b91f9b4e1ac5b06
Daniel Claborne <dmclaborne@gmail.com>
2021-10-05 17:10:44 -0700

    base functionality for e_meta option for custom filter

--- UI_elements/filter_UI.R
+++ UI_elements/filter_UI.R
@@ -62,11 +62,11 @@ list(
         object_samples <- attr(objects$filters[[i]], "omicsData")$f_data[, get_fdata_cname(attr(objects$filters[[i]], "omicsData"))]
         
         if ("f_data_keep" %in% names(objects$filters[[i]])) {
-          f_data_keep <- objects$filters[[i]]["f_data_keep"]
+          f_data_keep <- objects$filters[[i]][["f_data_keep"]]
           f_data_remove <- setdiff(object_samples, f_data_keep) %>% union(rmd_removed_samps) # ** include samples removed by rmd filter
         }
         else if ("f_data_remove" %in% names(objects$filters[[i]])) {
-          f_data_remove <- objects$filters[[i]]["f_data_remove"]
+          f_data_remove <- objects$filters[[i]][["f_data_remove"]]
           f_data_keep <- setdiff(object_samples, union(f_data_remove, rmd_removed_samps)) # **
         }
         
@@ -218,7 +218,7 @@ list(
           div(
             pickerInput(
               "fdata_customfilt_choices",
-              "Select Manually (dataset 1)",
+              "Filter samples (dataset 1)",
               choices = fdata_IDS,
               multiple = TRUE,
               options = list(`live-search` = TRUE, `actions-box` = TRUE)
@@ -230,7 +230,7 @@ list(
           div(
             pickerInput(
               "fdata_customfilt_choices_2",
-              "Select Manually (dataset 2)",
+              "Filter samples (dataset 2)",
               choices = fdata_IDS_2,
               multiple = TRUE,
               options = list(`live-search` = TRUE, `actions-box` = TRUE)
@@ -242,7 +242,7 @@ list(
     else {
       pickerInput(
         "fdata_customfilt_choices",
-        "Select Manually",
+        "Filter samples",
         choices = fdata_IDS,
         multiple = TRUE,
         options = list(`live-search` = TRUE, `actions-box` = TRUE)
@@ -385,11 +385,11 @@ list(
     mols1 = objects$uploaded_omicsData$e_data[,get_edata_cname(objects$uploaded_omicsData)]
     
     if(two_lipids()){
-      validate(need(!is.null(objects$uploaded_omicsData_2)), "No second object")
+      validate(need(!is.null(objects$uploaded_omicsData_2), "No second object"))
       mols2 = objects$uploaded_omicsData_2$e_data[,get_edata_cname(objects$uploaded_omicsData_2)]
       
       tagList(
-        tags$p("Select biomolecules to remove:"),
+        h5("Filter by data file identifiers:"),
         fluidRow(
           column(6,
              pickerInput(
@@ -412,11 +412,95 @@ list(
     } else {
       pickerInput(
         "edata_customfilt_remove_mols_1",
-        "Select biomolecules to remove:",
+        "Filter by data file identifiers:",
         choices = mols1,
         multiple = T,
         options = list(`live-search` = TRUE, `actions-box` = TRUE)
       ) 
+    }
+    
+  }),
+  
+  #'@details Pickers for which e-meta columns to use when filtering by e-meta
+  output$emeta_customfilt_which_col <- renderUI({
+    req(objects$uploaded_omicsData$e_meta)
+    
+    choices1 = colnames(objects$uploaded_omicsData$e_meta)
+    
+    if(two_lipids()) {
+      validate(need(!is.null(objects$uploaded_omicsData_2), "No second object"))
+      choices2 = colnames(objects$uploaded_omicsData_2)
+      
+      tagList(
+        tags$p("Filter by which columns:"),
+        div(
+          class = 'inline-wrapper-1',
+          pickerInput(
+            "emeta_customfilt_which_col_1",
+            choices = choices1
+          ),
+          pickerInput(
+            "emta_customfilt_which_col_2",
+            choices = mols2
+          ) 
+        )
+      )
+    } else {
+      pickerInput(
+        "emeta_customfilt_which_col_1",
+        "Filter by which column:",
+        choices = choices1
+      )
+    }
+    
+  }),
+  
+  #'@details Picker inputs for selecting which molecules to remove using e_data
+  #'custom filter.
+  output$emeta_customfilt_pickers<- renderUI({
+    validate(
+      need(objects$uploaded_omicsData$e_meta, "No biomolecule information found."),
+      need(input$emeta_customfilt_which_col_1, "Select which column to use.")
+    )
+    
+    choices1 <- objects$uploaded_omicsData$e_meta %>% 
+      purrr::pluck(input$emeta_customfilt_which_col_1)
+    
+    if(two_lipids()){
+      validate(
+        need(!is.null(objects$uploaded_omicsData_2$e_meta), "No biomolecule information for second data object."),
+        need(input$emeta_customfilt_which_col_2, "Select second identifier.")
+      )
+      choices2 <- objects$uploaded_omicsData_2$e_meta %>% 
+        purrr::pluck(input$emeta_customfilt_which_col_2)
+      
+      tagList(
+        tags$p("Filter which biomolecule information values:"),
+        div(
+          class = 'inline-wrapper-1',
+          pickerInput(
+            "emeta_customfilt_which_values_1",
+            choices = choices1,
+            multiple = T,
+            options = list(`live-search` = TRUE, `actions-box` = TRUE)
+          ),
+          pickerInput(
+            "emta_customfilt_which_values_2",
+            choices = choices2,
+            multiple = T,
+            options = list(`live-search` = TRUE, `actions-box` = TRUE)
+          ) 
+        )
+      )
+      
+    } else {
+      pickerInput(
+        "emeta_customfilt_which_values_1",
+        "Filter which biomolecule information values:",
+        choices = choices1,
+        multiple = T,
+        options = list(`live-search` = TRUE, `actions-box` = TRUE)
+      )
     }
     
   }),
