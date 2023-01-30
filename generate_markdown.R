e231e3fb8b63f58005bedfaeabd5c9c91d7ba655
Daniel Claborne <dmclaborne@gmail.com>
2023-01-30 14:04:56 -0800

    add two lipid markdown, non-stacked imdanova plots

--- generate_markdown.R
+++ generate_markdown.R
@@ -24,11 +24,14 @@ list(
         return(NULL)
       }
       
-      browser()
-          
       # Create a temporary directory and copy the report there
-      report <- file.path(tempdir(), "Abundance_Template.Rmd")
-      file.copy(file.path("www", "markdowns", "Abundance_Template.Rmd"), report, overwrite = TRUE)
+      if (two_lipids()) {
+        report <- file.path(tempdir(), "Abundance_Template_2lipids.Rmd")
+        file.copy(file.path("www", "markdowns", "Abundance_Template_2lipids.Rmd"), report, overwrite = TRUE)  
+      } else {
+        report <- file.path(tempdir(), "Abundance_Template.Rmd")
+        file.copy(file.path("www", "markdowns", "Abundance_Template.Rmd"), report, overwrite = TRUE) 
+      }
       
       # Rearrange names if rollup has happened 
       if (!is.null(objects$omicsData_pre_rollup)) {
@@ -36,24 +39,44 @@ list(
         omicStats <- objects$imdanova_res
         omicDataRoll <- objects$omicsData
         omicStatsRoll <- objects$peptide_imdanova_res
+        omicData_2 <- NULL
       } else {
         omicData <- objects$omicsData
+        omicData_2 <- objects$omicsData_2
         omicStats <- objects$imdanova_res
         omicDataRoll <- NULL
         omicStatsRoll <- NULL
       }
       
       # Pull the original edata, emeta, and fdata 
-      if (is.null(input$file_edata$datapath)) {edata <- NULL} else {edata <- read.csv(input$file_edata$datapath)}
-      if (is.null(input$file_emeta$datapath)) {edata <- NULL} else {emeta <- read.csv(input$file_emeta$datapath)}
-      if (is.null(input$file_fdata$datapath)) {edata <- NULL} else {fdata <- read.csv(input$file_fdata$datapath)}
+      if (two_lipids()) {
+        edata <- objects$uploaded_omicsData$e_data
+        edata_2 <- objects$uploaded_omicsData_2$e_data
+        
+        fdata <- f_data()
+        
+        emeta <- objects$uploaded_omicsData$e_meta
+        emeta_2 <- objects$uploaded_omicsData_2$e_meta
+      } else {
+        if (is.null(input$file_edata$datapath)) {edata <- NULL} else {edata <- read.csv(input$file_edata$datapath)}
+        if (is.null(input$file_emeta$datapath)) {emeta <- NULL} else {emeta <- read.csv(input$file_emeta$datapath)}
+        if (is.null(input$file_fdata$datapath)) {fdata <- NULL} else {fdata <- read.csv(input$file_fdata$datapath)}
+      
+        edata_2 <- fdata_2 <- emeta_2 <- NULL
+        
+      }
       
       # Set up specific parameters needed by the peptide data markdown
       params <- list(titleName = input$ReportName,
                      edata = edata,
                      emeta = emeta,
                      fdata = fdata,
+                     edata_2 = edata_2,
+                     emeta_2 = emeta_2,
                      omicData = omicData,
+                     omicData_2 = omicData_2,
+                     uploaded_omicData = objects$uploaded_omicsData,
+                     uploaded_omicData_2 = objects$uploaded_omicsData_2,
                      omicStats = omicStats,
                      omicDataRoll = omicDataRoll,
                      omicStatsRoll = omicStatsRoll,
