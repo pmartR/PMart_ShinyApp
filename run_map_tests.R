b6d25ea7c18b4d2838705f00b2ba911ba6cf92dd
Evan Glasscock <evan.glasscock@pnnl.gov>
2023-05-22 11:38:42 -0700

    Remove on.exit in MAP test runner

--- run_map_tests.R
+++ run_map_tests.R
@@ -1,11 +1,9 @@
 source("tests/map_test_setup.R")
 
-on.exit({
-  Sys.setenv("MAP_SHINYTEST"=0)
-})
-
 for (f in dir("tests/testthat")) {
   if (startsWith(f, "test_map_")) {
     shinytest2::test_app(filter = substring(f, nchar("test_") + 1, nchar(f) - nchar(".R")))
   }
 }
+
+Sys.setenv("MAP_SHINYTEST"=0)
